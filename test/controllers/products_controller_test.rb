d4b910bf61f94e688f71826e37eba618b784d8c0
Aidys Dongak <aidongak23@gmail.com>
2023-11-15 15:46:29 +0700

    Iteration C5: Caching of Partial Results

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select 'h2', 'Programming Ruby 1.9'
   end
 
   test "should get new" do
