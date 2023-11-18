cdd367d539353db6144fe3893a9e39ea2f147ec1
Aidys Dongak <aidongak23@gmail.com>
2023-11-18 11:31:47 +0700

    Iteration F1: Moving the cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,9 @@
 class StoreController < ApplicationController
   include SessionCounter
-  
+  include CurrentCart
+
   before_action :increase_session_counter
+  before_action :set_cart
 
   def index
     @products = Product.order(:title)
