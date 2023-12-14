874612c04bb20ee769376227a7dbdfe33e575f68
Aidys Dongak <aidongak23@gmail.com>
2023-12-14 16:42:50 +0700

    Iteration G3 Testing Our Javascript Functionality

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -11,17 +11,22 @@ class OrdersTest < ApplicationSystemTestCase
   end
 
   test "should create order" do
+    visit store_index_url
+
+    click_on 'Add to Cart', match: :first
+
+    click_on 'Checkout'
+
     visit orders_url
-    click_on "New order"
+    click_on 'New order'
 
     fill_in "Address", with: @order.address
     fill_in "Email", with: @order.email
     fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Create Order"
+    select @order.pay_type, from: 'Pay type'
+    click_on "Place Order"
 
-    assert_text "Order was successfully created"
-    click_on "Back"
+    assert_text "Thank you for your order."
   end
 
   test "should update Order" do
@@ -31,11 +36,10 @@ class OrdersTest < ApplicationSystemTestCase
     fill_in "Address", with: @order.address
     fill_in "Email", with: @order.email
     fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Update Order"
+    select @order.pay_type, from: 'Pay type'
+    click_on "Place Order"
 
     assert_text "Order was successfully updated"
-    click_on "Back"
   end
 
   test "should destroy Order" do
@@ -43,5 +47,43 @@ class OrdersTest < ApplicationSystemTestCase
     click_on "Destroy this order", match: :first
 
     assert_text "Order was successfully destroyed"
+  end
+
+  test 'check dynamic fields' do
+    visit store_index_url
+
+    click_on 'Add to Cart', match: :first
+
+    click_on 'Checkout'
+
+    assert has_no_field? 'Routing number'
+    assert has_no_field? 'Account number'
+    assert has_no_field? 'Credit card number'
+    assert has_no_field? 'Expiration date'
+    assert has_no_field? 'Po number'
+
+    select 'Check', from: 'Pay type'
+
+    assert has_field? 'Routing number'
+    assert has_field? 'Account number'
+    assert has_no_field? 'Credit card number'
+    assert has_no_field? 'Expiration date'
+    assert has_no_field? 'Po number'
+
+    select 'Credit card', from: 'Pay type'
+
+    assert has_no_field? 'Routing number'
+    assert has_no_field? 'Account_number'
+    assert has_field? 'Credit card number'
+    assert has_field? 'Expiration date'
+    assert has_no_field? 'Po number'
+
+    select 'Purchase order', from: 'Pay type'
+
+    assert has_no_field? 'Routing number'
+    assert has_no_field? 'Account_number'
+    assert has_no_field? 'Credit card number'
+    assert has_no_field? 'Expiration date'
+    assert has_field? 'Po number'
   end
 end
