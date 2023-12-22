b57745ab7e9e778767ff98646746843dc14d1da7
Aidys Dongak <aidongak23@gmail.com>
2023-12-22 18:20:54 +0700

    view

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,7 @@ class StoreController < ApplicationController
   include SessionCounter
   include CurrentCart
 
+  skip_before_action :authorize
   before_action :increase_session_counter
   before_action :set_cart
 
